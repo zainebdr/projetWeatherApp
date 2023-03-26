f72ec4a6c756754eac4f6d83b3041a09760dbe93
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-03-26 18:06:44 +0200

    added map interface

--- flask-app/main.py
+++ flask-app/main.py
@@ -7,5 +7,24 @@ app = Flask(__name__)
 def hello():
     return render_template("index.html")
 
+@app.route('/map')
+def map():
+    countries = [{
+        'country' : "tunisie",
+        'lat' : "",
+        'lon' :""
+    }]
+    cities = [{
+        'city' : "ariana",
+        'lat' : "36.862499",
+        'lon' :"10.195556"
+    },
+    {
+        'city' : "tunis",
+        'lat' : "33.886917",
+        'lon' :"9.537499"
+    }]
+    return render_template("map.html", data= {'countries':countries , 'cities' : cities})
+
 if __name__ == "__main__":
     app.run(debug=True)
