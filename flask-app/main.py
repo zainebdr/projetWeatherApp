64925ab168e29aa2321a8f60e407ecf613c7873f
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-03-27 02:13:12 +0200

    added listener to li (cities) on click

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,11 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    countries = [{
+    'country' : "tunisie",
+    'lat' : "",
+    'lon' :""
+    }]
+    cities = [{
+    'city' : "ariana",
+    'lat' : "36.862499",
+    'lon' :"10.195556"
+    },
+    {
+    'city' : "tunis",
+    'lat' : "33.886917",
+    'lon' :"9.537499"
+    }]
+    return render_template("index.html" , cities=cities)
+
+@app.route('/city-click',methods=['POST'])
+def cityClick():
+    json_data = request.get_json()
+    print(json_data)
+    return "clicked"
 
 @app.route('/map')
 def map():
