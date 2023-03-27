34badc36cbb1141fe09937383e62839f0cfae57b
Mady0101 <mrabetmohamedmahdi@gmail.com>
2023-03-27 03:41:03 +0200

    modif

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,33 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
+app.jinja_env.auto_reload = True
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
-
-
-@app.route('/')
-def hello():
-    countries = [{
+countries = [{
     'country' : "tunisie",
     'lat' : "",
     'lon' :""
     }]
-    cities = [{
+cities = [{
     'city' : "ariana",
     'lat' : "36.862499",
-    'lon' :"10.195556"
+    'lon' :"10.195556",
+    'weather' :"cloudy"
     },
     {
     'city' : "tunis",
     'lat' : "33.886917",
-    'lon' :"9.537499"
+    'lon' :"9.537499",
+    'weather':"sunny"
     }]
-    return render_template("index.html" , cities=cities)
+
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", cities=cities , city={'city' : "tunis",
+    'lat' : "33.886917",
+    'lon' :"9.537499",
+    'weather':"sunny"})
+
+@app.route('/<string:cityname>')
+def displayCityDetail(cityname):
+    
+    searchedcity = ""
+    for city in cities:
+        if( cityname== city['city']):
+            searchedcity = city 
+    return render_template("index.html" , cities=cities, city = searchedcity)
 
 @app.route('/city-click',methods=['POST'])
 def cityClick():
+    cities = [{
+    'city' : "ariana",
+    'lat' : "36.862499",
+    'lon' :"10.195556",
+    'weather' :"cloudy"
+    },
+    {
+    'city' : "tunis",
+    'lat' : "33.886917",
+    'lon' :"9.537499",
+    'weather':"sunny"
+    }]
     json_data = request.get_json()
     print(json_data)
-    return "clicked"
+    searchedcity = ""
+    for city in cities:
+        if(json_data['id'] == city['city']):
+            searchedcity = city
+    print("seached")
+    print(searchedcity)
+
+    return redirect("map.html" , cities=cities , city = searchedcity)
 
 @app.route('/map')
 def map():
